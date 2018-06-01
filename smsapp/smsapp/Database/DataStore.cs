c5c25a778d101466fd09ed7341600c655f4eb7ea
mebungler <mebungler@gmail.com>
2018-06-01 12:21:23 +0500

    Update

--- smsapp/smsapp/Database/DataStore.cs
+++ smsapp/smsapp/Database/DataStore.cs
@@ -2,40 +2,9 @@
 
 namespace smsapp
 {
-    public class DataStore : IDataStore
+    public class DataStore
     {
-
-
-
-        #region Interface Implementations
-
-        /// <summary>
-        /// Adds the user to the Database
-        /// </summary>
-        /// <param name="dataModel">The user to add</param>
-        /// <returns></returns>
-        public Task AddUserData(UserDataModel dataModel)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        /// Makes sure that database is created and set up
-        /// </summary>
-        /// <returns></returns>
-        public Task EnsureDataStoreAsyn()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        /// Gets the UserData
-        /// </summary>
-        /// <returns></returns>
-        public Task<UserDataModel> GetUserData()
-        {
-            throw new System.NotImplementedException();
-        } 
-        #endregion
+        // Create/Read/Update/Delete
+        
     }
 }
